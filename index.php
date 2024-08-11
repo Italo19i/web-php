a99a23fd0d2c500192d5d55cee96c92eab00b03b
Italo <ciol.profissional@gmail.com>
2024-08-11 17:13:23 +0000

    feat: 🎸 added my first php code
    code provided for ufrn course web

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Página PHP</title>
+  </head>
+  <body>
+    <?php echo 'O comando echo do PHP imprime esse texto dentro do documento<br/>'; ?>
+    Também é possível usar a tag curta <?= 'para mostrar esse texto' ?>
+    ou para exibir o resultado de 1 + 1 que é <?= 1 + 1 ?>.
+  </body>
+</html>
